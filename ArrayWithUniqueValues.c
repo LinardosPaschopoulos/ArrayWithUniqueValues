cfc65ec886043ca81477937e3f475bbe3fb38585
LinardosPaschopoulos <54861165+LinardosPaschopoulos@users.noreply.github.com>
2020-12-03 14:56:00 +0200

    Update ArrayWithUniqueValues.c

--- ArrayWithUniqueValues.c
+++ ArrayWithUniqueValues.c
@@ -1,53 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define x 2 //must be greater than 0
-#define y 2 //must be greater than 0
-#define z 2 //must be greater than 0
+#define x 50 //must be greater than 0
+#define y 50 //must be greater than 0
+#define z 50 //must be greater than 0
 
 int main(void)
 {
-	int MyArray[x][y][z],i,j,k,i_check,j_check,k_check,size;
+	int MyArray[x][y][z], i, j, k, i_check, j_check, k_check, size = x * y * z;
 	
-	size=x*y*z;
+	size;
+	srand (time(NULL));
 	
-	for(j=0;j<y;j++)
-	{
-	    for(i=0;i<x;i++)
-	    {
-			for(k=0;k<z;k++)
-			{	
-				MyArray[i][j][k]=rand()%size;
-		       	for(j_check=0;j_check<y;j_check++)						
-		        {
-					for(k_check=0;k_check<z;k_check++)
-					{
-						for(i_check=0;i_check<x;i_check++)
-				        {
-				        	while(MyArray[i][j][k]==MyArray[i_check][j_check][k_check])
-					        {	
-					        	if(i_check>=i&&j_check>=j&&k_check>=k)
+	for(i = 0; i < x; i++) {
+	    for(j = 0; j < y; j++){
+			for(k = 0; k < z; k++) {	
+				MyArray[i][j][k] = rand()%size;
+		       	for(i_check = 0; i_check < x; i_check++) {
+					for(j_check = 0; j_check < y; j_check++) {
+						for(k_check = 0; k_check < z; k_check++) {
+				        	while(MyArray[i][j][k] == MyArray[i_check][j_check][k_check]) {	
+					        	if(i_check >= i && j_check >= j && k_check >= k)
 					        		break;
-								MyArray[i][j][k]=rand()%size;
-					        	i_check=0;
-					        	j_check=0;
-								k_check=0;
+								MyArray[i][j][k] = rand()%size;
+					        	i_check = 0;
+					        	j_check = 0;
+								k_check = 0;
 							}
 						}
 					}
+				
 				}	
+			printf("%8.1d   " ,MyArray[i][j][k]);
 			}
 		}			
+		printf("\n");
 	}
-	
-	for(j=0;j<y;j++)
-	    for(k=0;k<z;k++)
-			{
-		    for(i=0;i<x;i++)
-		    printf("%8.1d   ",MyArray[i][j][k]);
-		    printf("\n\n");
-		    }
-	
+
 	system("pause");
 	return 0;
 }
