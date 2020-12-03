218a483de18fdcf488d8e34caacf31b27376f2eb
LinardosPaschopoulos <54861165+LinardosPaschopoulos@users.noreply.github.com>
2020-12-03 14:57:24 +0200

    Update ArrayWithUniqueValues.c

--- ArrayWithUniqueValues.c
+++ ArrayWithUniqueValues.c
@@ -8,23 +8,22 @@
 int main(void)
 {
 	int MyArray[x][y][z], i, j, k, i_check, j_check, k_check, size = x * y * z;
-	
-	size;
+
 	srand (time(NULL));
 	
 	for(i = 0; i < x; i++) {
-	    for(j = 0; j < y; j++){
+		for(j = 0; j < y; j++){
 			for(k = 0; k < z; k++) {	
 				MyArray[i][j][k] = rand()%size;
-		       	for(i_check = 0; i_check < x; i_check++) {
+			       	for(i_check = 0; i_check < x; i_check++) {
 					for(j_check = 0; j_check < y; j_check++) {
 						for(k_check = 0; k_check < z; k_check++) {
-				        	while(MyArray[i][j][k] == MyArray[i_check][j_check][k_check]) {	
-					        	if(i_check >= i && j_check >= j && k_check >= k)
-					        		break;
+					        	while(MyArray[i][j][k] == MyArray[i_check][j_check][k_check]) {	
+						        	if(i_check >= i && j_check >= j && k_check >= k)
+						        		break;
 								MyArray[i][j][k] = rand()%size;
-					        	i_check = 0;
-					        	j_check = 0;
+					        		i_check = 0;
+					        		j_check = 0;
 								k_check = 0;
 							}
 						}
